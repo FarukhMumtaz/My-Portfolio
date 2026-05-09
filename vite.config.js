776ff860829dec4f4d44bac05867b565463bee81
FarukhMumtaz <farukhofficial555@gmail.com>
2026-05-10 00:05:04 +0500

    Updated Config to fix form error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Agar error aa raha hai toh ye block add kar sakte ho (Optional but helpful)
+  define: {
+    'process.env': {}
+  }
 })
